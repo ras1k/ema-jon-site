088788a50e0bb6c18d5c9089aeac3fbe630fb0d6
ras1k <rasikabdullah@gmail.com>
2023-05-07 01:27:01 +0600

    several router added

---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <p>xxxxxxxxxxxx</p>
+        </div>
+    );
+};
+
+export default Inventory;
---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <p>lmaoooooooo</p>
+        </div>
+    );
+};
+
+export default Orders;
