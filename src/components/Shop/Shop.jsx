0de04bfc351a964c3c9a29b7afdeaea1f6324cd1
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-13 19:18:04 +0600

    margin  added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -31,97 +31,111 @@ const Shop = () => {
             const data = await response.json();
             setProducts(data);
         }
-            fetchData();
-        }, [currentPage, itemsPerPage]);
+        fetchData();
+    }, [currentPage, itemsPerPage]);
 
-useEffect(() => {
-    const storedCart = getShoppingCart();
-    const savedCart = [];
-    for (const id in storedCart) {
-        const addedProduct = products.find(product => product._id === id)
-        if (addedProduct) {
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            savedCart.push(addedProduct);
-        }
-    }
-    setCart(savedCart);
-}, [products]);
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const ids = Object.keys(storedCart);
 
-const handleAddToCart = (product) => {
-    let newCart = [];
-    // const newCart = [...cart, product];
-    const exists = cart.find(pd => pd._id == product._id);
-    if (!exists) {
-        product.quantity = 1;
-        newCart = [...cart, product];
-    } else {
-        exists.quantity = exists.quantity + 1;
-        const remaining = cart.filter(pd => pd._id !== product._id);
-        newCart = [...remaining, exists];
-    }
-    setCart(newCart);
-    addToDb(product._id);
-    console.log(product._id);
-}
-
-const clearCart = () => {
-    setCart([]);
-    deleteShoppingCart();
-}
-
-const options = [5, 10, 20]
-const handleSelectChange = (event) => {
-    setItemsPerPage(parseInt(event.target.value))
-    setCurrentPage(0)
-}
-
-return (
-    <>
-        <div className='shop-container'>
-            <div className='products-container'>
-                {
-                    products.map(product => <Product
-                        key={product._id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
-            </div>
-            <div className='cart-container'>
-                <Cart
-                    cart={cart}
-                    clearCart={clearCart}
-                >
-                    <Link to="/orders" className='proceed-link'>
-                        <button className='btn-proceed'>Review Order</button>
-                    </Link>
-                </Cart>
-            </div>
-
-        </div>
-        <div className="pagination">
-            <p>Current Page: {currentPage}</p>
-            {
-                pageNumbers.map(number => <button
-                    key={number}
-                    className={currentPage === number ? 'seleted' : ''}
-                    onClick={() => setCurrentPage(number)}
-                >
-                    {number}
-                </button>)
+        fetch(`http://localhost:5000/productsByIds`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(ids)
+        })
+            .then(res => res.json())
+            .then(cartProducts => {
+                const savedCart = [];
+                    for (const id in storedCart) {
+                const addedProduct = cartProducts.find(product => product._id === id)
+                    if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
             }
-            <select value={itemsPerPage} onChange={handleSelectChange}>
-                {options.map(option => (
-                    <option key={option} value={option}>
-                        {option}
-                    </option>
-                ))}
+        }
+        setCart(savedCart);
+            })
 
-            </select>
-        </div >
-    </>
-);
+        
+    }, []);
+
+    const handleAddToCart = (product) => {
+        let newCart = [];
+        // const newCart = [...cart, product];
+        const exists = cart.find(pd => pd._id == product._id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        } else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd._id !== product._id);
+            newCart = [...remaining, exists];
+        }
+        setCart(newCart);
+        addToDb(product._id);
+        console.log(product._id);
+    }
+
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
+
+    const options = [5, 10, 15, 20]
+    const handleSelectChange = (event) => {
+        setItemsPerPage(parseInt(event.target.value))
+        setCurrentPage(0)
+    }
+
+    return (
+        <>
+            <div className='shop-container'>
+                <div className='products-container'>
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
+                <div className='cart-container'>
+                    <Cart
+                        cart={cart}
+                        clearCart={clearCart}
+                    >
+                        <Link to="/orders" className='proceed-link'>
+                            <button className='btn-proceed'>Review Order</button>
+                        </Link>
+                    </Cart>
+                </div>
+
+            </div>
+            <div className="pagination">
+                <p>Current Page: {currentPage}</p>
+                {
+                    pageNumbers.map(number => <button
+                        key={number}
+                        className={currentPage === number ? 'seleted' : 'gap'}
+                        onClick={() => setCurrentPage(number)}
+                    >
+                        {number}
+                    </button>)
+                }
+                <select value={itemsPerPage} onChange={handleSelectChange}>
+                    {options.map(option => (
+                        <option key={option} value={option} >
+                            {option}
+                        </option>
+                    ))}
+
+                </select>
+            </div >
+        </>
+    );
 };
 
 export default Shop;
--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,10 +1,20 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
-    const products = await loadedProducts.json();
-
     const storedCart = getShoppingCart();
+    console.log(storedCart);
+    const ids = Object.keys(storedCart);
+    console.log(ids);
+    const loadedProducts = await fetch(`http://localhost:5000/productsByIds`, {
+        method: 'POST',
+        headers: {
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify(ids)
+    });
+    const products = await loadedProducts.json();
+    console.log()
+
     const savedCard = [];
     for (const id in storedCart) {
         const addedProduct = products.find(pd => pd._id === id);
